b3204982f785dd55985a00ca91281f5e41b3f724
jsw <jsw48929@gmail.com>
2023-04-28 20:26:27 +0800

    new

--- src/main/resources/sql/teacherLoginInfo.sql
+++ src/main/resources/sql/teacherLoginInfo.sql
@@ -2,7 +2,7 @@
 drop table if exists teacherLoginInfo;
 
 create table teacherLoginInfo(
-
 teacherId varchar(50) primary key,
-passWord varchar(500) not null
+passWord varchar(500) not null,
+powerToClass varchar(20) default null
 );
